878750f0461e2430326b35389f9bd10396ee1b2d
johncage <573697439@qq.com>
2022-01-27 16:55:28 +0800

    REF：准备重构

--- torch_lib/data/context.py
+++
@@ -1,9 +0,0 @@
-from torch_lib.utils import build_from_dict
-from typing import Optional
-
-
-class DataProviderContext:
-
-    def __init__(self, **kwargs):
-        self.epoch: Optional[int] = None
-        build_from_dict(self, kwargs, required_params=[])
