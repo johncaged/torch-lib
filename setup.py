313425b403b52d15cd475ce64cfa194697763d44
johncage <573697439@qq.com>
2022-03-23 17:28:38 +0800

    REF: switch gitee repository to github repository.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     include_package_data=False,
     entry_points={},
     install_requires=[],
-    url='https://gitee.com/johncaged/torch-lib',
+    url='https://github.com/johncaged/torch-lib',
     license='GNU General Public License v2.0',
     author='Zikang Liu',
     author_email='573697439@qq.com',
